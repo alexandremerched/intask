1fa92cfda7d99ae782e68f392563485f15be51a4
artismyname <4lex4andre@gmail.com>
2022-10-25 11:37:16 -0500

    feat: add animation fade-in and add default color for outline globally

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,11 +17,16 @@ module.exports = {
         'fade-out': {
           '0%': { opacity: '1' },
           '100%': { opacity: '0' }
+        },
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
         }
       },
       animation: {
         'slide-right': 'slide-right 0.3s',
-        'fade-out': 'fade-out ease 0.3s'
+        'fade-out': 'fade-out ease 0.3s',
+        'fade-in': 'fade-in ease 0.2s'
       }
     },
   },
